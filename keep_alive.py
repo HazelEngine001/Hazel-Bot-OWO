9e7a560140b9ef07e3a838adfed1081eae932a13
HazelEngine001 <samdragon1501@gmail.com>
2025-07-14 16:54:28 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 import threading
 
@@ -8,7 +9,9 @@ def home():
     return "Bot is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    # Lấy cổng từ môi trường, nếu không có thì mặc định là 8080
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = threading.Thread(target=run)
